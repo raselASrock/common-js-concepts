5f59ebfcaed5944d97825b28f2bdb2a084753df1
raselASrock <mdraselm413@gmail.com>
2022-09-07 20:57:09 +0600

    type js added

--- types.js
+++ types.js
@@ -1,2 +1,34 @@
+
+// Strongly Typed Programming Language
+
 // int a = 5;
-// String b = 'Alim Halim Khalim'
+// string b = 'Alim Halim Khalim'
+// bool c = True 
+// object student = {name: 'noya daman', id: 1205, }
+// int [] numbers = [45,22,95,76,18,92,46]
+// string[] friends = ['Abul', 'Babul', 'Kabul', 'Habul']
+
+// Dynamic Typed Programming Language
+
+/* const a = 5;
+const b = 'Shamcu Kopai na Ekhn r';
+const c = true */
+
+// Non- Primitive
+
+/* const ages = [45, 85, 15, 96];
+const student = {id: 25, class: 7};
+console.log( typeof a, typeof b, typeof c, typeof ages, typeof student ); */
+
+
+let x = 5;
+let y = x;
+console.log(x,y);
+y = 7;
+console.log(x,y);
+
+let p = {job: 'web developer'}
+let q = p
+console.log(p,q);
+q.job = 'backend'
+console.log(p,q);
