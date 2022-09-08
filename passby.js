b59dc912d4a1308c875ab1e5cc27374573ffb570
raselASrock <mdraselm413@gmail.com>
2022-09-08 23:16:42 +0600

    quize

--- passby.js
+++ passby.js
@@ -25,3 +25,26 @@ console.log(student1, student2);
 
 makeMovie(student1, student2);
 console.log(student1, student2);
+
+let p='Javascript'; 
+let q=p; 
+p='React'; 
+console.log(q); 
+
+const isTrue='false'; 
+if(!isTrue){
+console.log('hello');
+} else {
+console.log('world'); 
+}
+
+if ("2" === 2) {
+    console.log("Inside if");
+   } else {
+    console.log("Inside else");
+   }
+
+   function work(x, y = 4) {
+    return x + y;
+   }
+   console.log(work(32);
