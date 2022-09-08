57871820a1c1ee92e23353efc1035664d01bcedf
raselASrock <mdraselm413@gmail.com>
2022-09-08 17:08:40 +0600

    scope added

---
+++ scope.js
@@ -0,0 +1,16 @@
+function add(a,b){
+    const total = a + b;
+    if(b > 5){
+        const sum = 25 + a + b;
+        console.log(sum);
+    }
+    else{
+        const sum = 5 + a + b ;
+        var current = sum;
+    }
+    console.log(current);
+    return total
+}
+// console.log(total);
+
+add(5,7)
